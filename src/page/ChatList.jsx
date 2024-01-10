294e1d15a53c49da11354a9f3a7c3199400d6475
INSEONG-PC\정인성 <twin2688@naver.com>
2024-01-10 15:13:09 +0900

    채팅창, 채팅리스트(수정) 구현완료

--- src/page/ChatList.jsx
+++ src/page/ChatList.jsx
@@ -3,93 +3,96 @@ import styled from "styled-components";
 import MenuBar from "../component/MenuBar";
 import LogoBar from "../component/LogoBar";
 import { IoSettingsOutline } from "react-icons/io5";
+import { FaChevronLeft } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function ChatList() {
+  const navigate = useNavigate();
+
   return (
     <>
-      <LogoBar />
       <ChatBox>
-        <div className="title">
-          <div>Chat Message</div>
+        <div className="HeaderBar">
+          <FaChevronLeft
+            className="goBack-icon"
+            onClick={() => {
+              navigate(-1);
+            }}
+          />
+          <div className="title">
+            <div>인성님의 채팅방</div>
+          </div>
           <div>
             <IoSettingsOutline className="icon" />
           </div>
         </div>
-        <div className="chatBox">
-          <div className="chat-card">
-            <div className="card-image"></div>
-            <div className="user-box">
-              <div className="user-name">이름</div>
-              <div className="user-last">마지막 내용</div>
+        <div className="chat-container">
+          <Link to="/chatroom">
+            <div className="chatBox">
+              <div className="chat-card">
+                <div className="card-image"></div>
+                <div className="user-box">
+                  <div className="user-name">이름</div>
+                  <div className="user-last">마지막 내용</div>
+                </div>
+                <div className="time-box">
+                  <div className="time">13:30</div>
+                  <div className="message-count">1</div>
+                </div>
+              </div>
             </div>
-            <div className="time-box">
-              <div className="time">13:30</div>
-              <div className="notice">1</div>
-            </div>
-          </div>
-        </div>
-        <div className="chatBox">
-          <div className="chat-card">
-            <div className="card-image"></div>
-            <div className="user-box">
-              <div className="user-name">이름</div>
-              <div className="user-last">마지막 내용</div>
-            </div>
-            <div className="time-box">
-              <div className="time">13:30</div>
-              <div className="notice">1</div>
-            </div>
-          </div>
-        </div>
-        <div className="chatBox">
-          <div className="chat-card">
-            <div className="card-image"></div>
-            <div className="user-box">
-              <div className="user-name">이름</div>
-              <div className="user-last">마지막 내용</div>
-            </div>
-            <div className="time-box">
-              <div className="time">13:30</div>
-              <div className="notice">1</div>
+          </Link>
+
+          <div className="chatBox">
+            <div className="chat-card">
+              <div className="card-image"></div>
+              <div className="user-box">
+                <div className="user-name">이름</div>
+                <div className="user-last">마지막 내용</div>
+              </div>
+              <div className="time-box">
+                <div className="time">13:30</div>
+                <div className="message-count">1</div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="chatBox">
-          <div className="chat-card">
-            <div className="card-image"></div>
-            <div className="user-box">
-              <div className="user-name">이름</div>
-              <div className="user-last">마지막 내용</div>
-            </div>
-            <div className="time-box">
-              <div className="time">13:30</div>
-              <div className="notice">1</div>
-            </div>
-          </div>
-        </div>
-        <div className="chatBox">
-          <div className="chat-card">
-            <div className="card-image"></div>
-            <div className="user-box">
-              <div className="user-name">이름</div>
-              <div className="user-last">마지막 내용</div>
-            </div>
-            <div className="time-box">
-              <div className="time">13:30</div>
-              <div className="notice">1</div>
+          <div className="chatBox">
+            <div className="chat-card">
+              <div className="card-image"></div>
+              <div className="user-box">
+                <div className="user-name">이름</div>
+                <div className="user-last">마지막 내용</div>
+              </div>
+              <div className="time-box">
+                <div className="time">13:30</div>
+                <div className="message-count">1</div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="chatBox">
-          <div className="chat-card">
-            <div className="card-image"></div>
-            <div className="user-box">
-              <div className="user-name">이름</div>
-              <div className="user-last">마지막 내용</div>
+          <div className="chatBox">
+            <div className="chat-card">
+              <div className="card-image"></div>
+              <div className="user-box">
+                <div className="user-name">이름</div>
+                <div className="user-last">마지막 내용</div>
+              </div>
+              <div className="time-box">
+                <div className="time">13:30</div>
+                <div className="message-count">1</div>
+              </div>
             </div>
-            <div className="time-box">
-              <div className="time">13:30</div>
-              <div className="notice">1</div>
+          </div>
+          <div className="chatBox">
+            <div className="chat-card">
+              <div className="card-image"></div>
+              <div className="user-box">
+                <div className="user-name">이름</div>
+                <div className="user-last">마지막 내용</div>
+              </div>
+              <div className="time-box">
+                <div className="time">13:30</div>
+                <div className="message-count">1</div>
+              </div>
             </div>
           </div>
         </div>
@@ -102,13 +105,22 @@ export default function ChatList() {
 const ChatBox = styled.div`
   margin: 0px 10px;
   padding-bottom: 80px;
+
+  .HeaderBar {
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .goBack-icon {
+    font-size: 1.2rem;
+    width: 30px;
+  }
   .title {
     padding: 10px;
     font-weight: 700;
-    font-size: 1.4rem;
-    color: var(--sub-color);
+    font-size: 1.2rem;
     display: flex;
-    justify-content: space-between;
     align-items: center;
   }
 
@@ -125,7 +137,8 @@ const ChatBox = styled.div`
   }
 
   .icon {
-    font-size: 1.7rem;
+    font-size: 1.4rem;
+    width: 30px;
   }
 
   .card-image {
@@ -146,30 +159,33 @@ const ChatBox = styled.div`
     color: #606060;
   }
 
-  .time-box {
+  .time {
     position: absolute;
     right: 0;
+    margin: 10px;
     top: 0;
-    margin: 20px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .time {
-    margin-bottom: 5px;
-  }
-
-  .notice {
     font-weight: 700;
-    background-color: red;
-    color: #fff;
+  }
+
+  .message-count {
+    position: absolute;
+    right: 0;
+    margin: 10px;
+    bottom: 0;
+    font-weight: 700;
+    background-color: var(--point-color);
+    padding: 5px;
+    width: 20px;
+    height: 20px;
     border-radius: 50%;
-    width: 30px;
-    height: 30px;
     display: flex;
-    align-items: center;
     justify-content: center;
+    align-items: center;
+  }
+
+  .chat-container {
+    background-color: var(--hover-gray);
+    border-radius: 10px;
+    box-shadow: 0 0 2px rgba(0, 0, 0, 0.05), 4px 12px 36px rgba(0, 0, 0, 0.05);
   }
 `;
---
+++ src/page/ChatRoom.jsx
@@ -0,0 +1,167 @@
+import React from "react";
+import styled from "styled-components";
+import MenuBar from "../component/MenuBar";
+import { FaChevronLeft } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { CiLogout } from "react-icons/ci";
+import { FaArrowUp } from "react-icons/fa";
+export default function ChatRoom() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Container>
+        <div className="HeaderBar">
+          <FaChevronLeft
+            className="icon"
+            onClick={() => {
+              navigate(-1);
+            }}
+          />
+          <div className="title">
+            <div>닉네임</div>
+          </div>
+          <div>
+            <CiLogout className="icon" />
+          </div>
+        </div>
+        <div className="chat-room">
+          <div className="my-chat">
+            <div className="my-chatmessage">안녕하세요</div>
+            <div className="time">23:30</div>
+          </div>
+          <div className="search-box">
+            <div>
+              <FaArrowUp className="search-icon" />
+            </div>
+            <input
+              type="text"
+              className="search-input"
+              placeholder="메시지를 입력하세요."
+            />
+          </div>
+        </div>
+      </Container>
+
+      <MenuBar />
+    </>
+  );
+}
+
+const Container = styled.div`
+  height: 90vh;
+  .HeaderBar {
+    height: 10%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .icon {
+    font-size: 1.2rem;
+    width: 30px;
+  }
+
+  .title {
+    padding: 10px;
+    font-weight: 700;
+    font-size: 1.2rem;
+    display: flex;
+    align-items: center;
+  }
+
+  .chat-room {
+    box-shadow: 0 0 2px rgba(0, 0, 0, 0.05), 4px 12px 36px rgba(0, 0, 0, 0.05);
+    margin: 0 10px;
+    height: 90%;
+    position: relative;
+  }
+
+  .search-box {
+    display: flex;
+    align-items: center;
+    height: 40px;
+    background-color: var(--hover-gray);
+    border-radius: 10px;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+  }
+
+  .you-profile {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    border: 1px solid var(--dark-gray);
+  }
+
+  .search-input {
+    width: 80%;
+    height: 100%;
+    background-color: var(--hover-gray);
+    border-radius: 10px;
+    background-color: rgba(255, 255, 255, 0);
+    outline: none;
+    padding-left: 7px;
+  }
+
+  .search-icon {
+    font-size: 1.3rem;
+    margin-left: 7px;
+    color: var(--point-color);
+  }
+
+  .one-box {
+    display: flex;
+    justify-content: center;
+    margin-right: 10px;
+  }
+
+  .two-box {
+    width: 80%;
+  }
+
+  .three-box {
+    display: flex;
+  }
+
+  .time {
+    display: flex;
+    align-items: flex-end;
+  }
+
+  .youchat {
+    display: flex;
+    padding-top: 10px;
+  }
+
+  .my-chat {
+    display: flex;
+    padding-top: 10px;
+    flex-direction: row-reverse;
+  }
+
+  .you-nickname {
+    font-weight: 700;
+    margin-bottom: 5px;
+  }
+
+  .you-chatmessage {
+    padding: 20px;
+    border-radius: 0px 30px 30px 30px;
+    background-color: var(--hover-gray);
+    display: inline-block;
+    line-height: 1.2;
+    letter-spacing: 1px;
+  }
+
+  .my-chatmessage {
+    padding: 20px;
+    border-radius: 30px 0px 30px 30px;
+    background-color: var(--point-color);
+    font-weight: 700;
+    display: inline-block;
+    line-height: 1.2;
+    letter-spacing: 1px;
+  }
+`;
