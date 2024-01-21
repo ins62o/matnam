a80ff3b21f8ead2ae85bf213b6b25ecf65ad2254
INSEONG-PC\정인성 <twin2688@naver.com>
2024-01-21 15:40:06 +0900

    좋아요 기능 구현

--- src/Firebase/actionFn.js
+++ src/Firebase/actionFn.js
@@ -13,7 +13,7 @@ import { db } from "../firebase";
 import { useQueryClient } from "@tanstack/react-query";
 
 // 조회수 증가
-export const seeIncrease = async ({ id }) => {
+export const IncraseSee = async ({ id }) => {
   const recipeDocRef = await getDoc(doc(db, "recipe", id));
   const seeField = recipeDocRef.data().see;
   const updateData = {
@@ -21,4 +21,23 @@ export const seeIncrease = async ({ id }) => {
   };
   // updateDoc 함수가 반환하는 Promise를 기다림
   await updateDoc(doc(db, "recipe", id), updateData);
+};
+
+export const IncreaseHeart = async ({ recipeId, nickname }) => {
+  const recipeDocRef = await getDoc(doc(db, "recipe", recipeId.id));
+  const likeField = recipeDocRef.data().heart;
+
+  if (likeField.includes(nickname)) {
+    const heart = likeField.filter((item) => item !== nickname);
+    const updateData = {
+      heart,
+    };
+    await updateDoc(doc(db, "recipe", recipeId.id), updateData);
+  } else {
+    const heart = [...likeField, nickname];
+    const updateData = {
+      heart,
+    };
+    await updateDoc(doc(db, "recipe", recipeId.id), updateData);
+  }
 };
