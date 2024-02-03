74da563571d9ec2f7e3c23141d64f645646b56c6
INSEONG-PC\정인성 <twin2688@naver.com>
2024-02-03 20:54:02 +0900

    react lazy 로 바꿔 보기 ( 2.6초 -> 1.5초 )

--- src/App.js
+++ src/App.js
@@ -1,66 +1,75 @@
-import "./App.css";
+// 외부 - import
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./page/Login";
-import SignUp from "./page/SignUp";
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import RecipeHome from "./page/Recipe/RecipeHome";
-import ScrollToTop from "./services/ScrollToTop";
-import RecipeWriteOne from "./page/Recipe/RecipeWriteOne";
-import RecipeWriteTwo from "./page/Recipe/RecipeWriteTwo";
-import RecipeWriteThree from "./page/Recipe/RecipeWriteThree";
-import RecipeEditOne from "./page/Recipe/RecipeEditOne";
-import RecipeEditTwo from "./page/Recipe/RecipeEditTwo";
-import RecipeEditThree from "./page/Recipe/RecipeEditThree";
-import RecipeFeed from "./page/Recipe/RecipeFeed";
-import RecipeDetail from "./page/Recipe/RecipeDetail";
-import MyPage from "./page/Mypage/MyPage";
+import { lazy, Suspense } from "react";
 
+// 내부 - import
+import "./App.css";
+import Loading from "./page/Loading";
+import ScrollToTop from "./services/ScrollToTop";
 const queryClient = new QueryClient();
+const Login = lazy(() => import("./page/Login"));
+const SignUp = lazy(() => import("./page/SignUp"));
+const RecipeHome = lazy(() => import("./page/Recipe/RecipeHome"));
+const RecipeWriteOne = lazy(() => import("./page/Recipe/RecipeWriteOne"));
+const RecipeWriteTwo = lazy(() => import("./page/Recipe/RecipeWriteTwo"));
+const RecipeWriteThree = lazy(() => import("./page/Recipe/RecipeWriteThree"));
+const RecipeEditOne = lazy(() => import("./page/Recipe/RecipeEditOne"));
+const RecipeEditTwo = lazy(() => import("./page/Recipe/RecipeEditTwo"));
+const RecipeEditThree = lazy(() => import("./page/Recipe/RecipeEditThree"));
+const RecipeFeed = lazy(() => import("./page/Recipe/RecipeFeed"));
+const RecipeDetail = lazy(() => import("./page/Recipe/RecipeDetail"));
+const MyPage = lazy(() => import("./page/Mypage/MyPage"));
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <>
-        <RecoilRoot>
-          <BrowserRouter>
-            <ScrollToTop />
-            <Routes>
-              <Route path="/" element={<RecipeHome />}></Route>
-              <Route
-                path="/recipeFeed/:category"
-                element={<RecipeFeed />}
-              ></Route>
-              <Route path="/recipeFeed/" element={<RecipeFeed />}></Route>
-              <Route path="/RecipeWrite" element={<RecipeWriteOne />}></Route>
-              <Route
-                path="/RecipeWriteTwo"
-                element={<RecipeWriteTwo />}
-              ></Route>
-              <Route
-                path="/RecipeWriteThree"
-                element={<RecipeWriteThree />}
-              ></Route>
+        <Suspense fallback={<Loading />}>
+          <RecoilRoot>
+            <BrowserRouter>
+              <ScrollToTop />
+              <Routes>
+                <Route path="/" element={<RecipeHome />}></Route>
+                <Route
+                  path="/recipeFeed/:category"
+                  element={<RecipeFeed />}
+                ></Route>
+                <Route path="/recipeFeed/" element={<RecipeFeed />}></Route>
+                <Route path="/RecipeWrite" element={<RecipeWriteOne />}></Route>
+                <Route
+                  path="/RecipeWriteTwo"
+                  element={<RecipeWriteTwo />}
+                ></Route>
+                <Route
+                  path="/RecipeWriteThree"
+                  element={<RecipeWriteThree />}
+                ></Route>
 
-              <Route path="/RecipeEdit/:id" element={<RecipeEditOne />}></Route>
-              <Route
-                path="/RecipeEditTwo/:id"
-                element={<RecipeEditTwo />}
-              ></Route>
-              <Route
-                path="/RecipeEditThree/:id"
-                element={<RecipeEditThree />}
-              ></Route>
-              <Route
-                path="/RecipeDetail/:id"
-                element={<RecipeDetail />}
-              ></Route>
-              <Route path="/Login" element={<Login />}></Route>
-              <Route path="/SignUP" element={<SignUp />}></Route>
-              <Route path="/Mypage/:nickname" element={<MyPage />}></Route>
-            </Routes>
-          </BrowserRouter>
-        </RecoilRoot>
+                <Route
+                  path="/RecipeEdit/:id"
+                  element={<RecipeEditOne />}
+                ></Route>
+                <Route
+                  path="/RecipeEditTwo/:id"
+                  element={<RecipeEditTwo />}
+                ></Route>
+                <Route
+                  path="/RecipeEditThree/:id"
+                  element={<RecipeEditThree />}
+                ></Route>
+                <Route
+                  path="/RecipeDetail/:id"
+                  element={<RecipeDetail />}
+                ></Route>
+                <Route path="/Login" element={<Login />}></Route>
+                <Route path="/SignUP" element={<SignUp />}></Route>
+                <Route path="/Mypage/:nickname" element={<MyPage />}></Route>
+              </Routes>
+            </BrowserRouter>
+          </RecoilRoot>
+        </Suspense>
       </>
     </QueryClientProvider>
   );
